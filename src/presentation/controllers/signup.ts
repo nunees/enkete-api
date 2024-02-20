1176bd81ba975b3397bdc2a78e458d8ac3d2f7be
nunees <felipe_silva_nunes@hotmail.com>
2024-02-20 20:28:34 -0300

    feat: ensure SignUpController returns 400 if password confirmation fails

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -18,6 +18,10 @@ export class SignUpController implements Controller {
         }
       }
 
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
+
       // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
       const isValid = this.emailValidator.isValid(httpRequest.body.email)
       if (!isValid) {
