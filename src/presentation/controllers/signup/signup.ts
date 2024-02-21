8febd2b0cbb5acc1b08977b89d77576c6d510036
nunees <felipe_silva_nunes@hotmail.com>
2024-02-21 14:27:03 -0300

    feat: ensure SignUpController 200 if valid data is provided

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -32,15 +32,15 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParamError('email'))
       }
 
-      this.addAccount.add({
+      const account = this.addAccount.add({
         name,
         email,
         password
       })
 
       return {
-        statusCode: 400,
-        body: new InvalidParamError('error')
+        statusCode: 200,
+        body: account
       }
     } catch (error) {
       return serverError()
