5f35cd59aea60fb4e782184536773aa84350d729
nunees <felipe_silva_nunes@hotmail.com>
2024-04-04 16:01:24 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,16 +2,19 @@ import { type Authentication, type AuthenticationModel } from "../../../domain/u
 import { type LoadAccountByEmailRepository } from "../../protocols/db/load-account-by-email-repository";
 import { type HashComparer } from "../../protocols/criptography/hash-comparer";
 import { type TokenGenerator } from "../../protocols/criptography/token-generator";
+import { type UpdateAccessTokenReposiory } from "../../protocols/db/update-access-token-repository";
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   private readonly hashComparer: HashComparer
   private readonly tokenGenerator: TokenGenerator
+  private readonly updateAccessTokenReposiory: UpdateAccessTokenReposiory
 
-  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer, tokenGenerator: TokenGenerator) {
+  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer, tokenGenerator: TokenGenerator, updateAccessTokenReposiory: UpdateAccessTokenReposiory) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashComparer = hashComparer
     this.tokenGenerator = tokenGenerator
+    this.updateAccessTokenReposiory = updateAccessTokenReposiory
   }
 
   async auth (authentication: AuthenticationModel): Promise<string> {
@@ -20,6 +23,7 @@ export class DbAuthentication implements Authentication {
       const isValid = await this.hashComparer.compare(authentication.password, account.password)
       if (isValid) {
         const accessToken = await this.tokenGenerator.generate(account.id)
+        await this.updateAccessTokenReposiory.update(account.id, accessToken)
         return accessToken
       }
     }
