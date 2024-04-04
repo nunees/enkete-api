6b025f63efec475a784911823ffac465cd68cccf
nunees <felipe_silva_nunes@hotmail.com>
2024-04-04 14:44:45 -0300

    refactor: move data layer interface to folders

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,7 +1,7 @@
 import { type HttpRequest, type Controller, type HttpResponse } from "../../presentation/protocols"
 import { LogControllerDecorator } from "./log"
 import { serverError, ok } from "../../presentation/helpers/http/http-helper"
-import { type LogErrorRepository } from "../../data/protocols/log-error-repository"
+import { type LogErrorRepository } from "../../data/protocols/db/log-error-repository"
 import { type AccountModel } from "../../domain/models/account"
 
 interface SutTypes {
